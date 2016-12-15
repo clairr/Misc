c2afe5fa89732216fd2f6d4704a3941f72f7ace4
clairr <clair.rios@gmail.com>
2016-12-15 19:19:45 -0200

    Tested multiple calls of request

--- js/testes.js
+++ js/testes.js
@@ -1,11 +1,21 @@
 var ultimo="http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?f_megasena="
-var primeiro= "http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso=1"
+var primeiro= "http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso="
+
+var concursos={lista:[]};
+var ultimoErro;
+var totalErros=0;
 
 var request = require('request');
-request({uri: primeiro, jar:true, encoding: 'latin1'}, function(error, req, body){
-  if( !error ) {
-  	console.log(body)
+for (i=1; i<40; i++) {
+	request({uri: primeiro+i, jar:true, encoding: 'latin1'}, function(error, req, data){
+		if( !error ) {
+			console.log("processando concurso "+ data.substr(0, data.indexOf('|')));
+			concursos.lista.push(data.substr(0, data.indexOf('|')));
 
-  };	
-});
+		} else {
+			totalErros+=1;
+			ultimoErro=error;
+		};	
+	});
+};
 
