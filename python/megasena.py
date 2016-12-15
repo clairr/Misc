4c2316dba4d0adba7eb354c463778578d01e407f
Clair <clair@mpf.mp.br>
2016-12-15 14:06:30 -0200

    Added latest megasena.py

--- python/megasena.py
+++ python/megasena.py
@@ -1,62 +1,55 @@
 #!/usr/bin/env python
-import urllib2
+from __future__ import print_function
 import pickle
+import requests
 from random import randrange
 ARQUIVO='sort.txt'
 
 class Concurso():
-  
+
   def __init__(self, pconcurso, pdata, pdezenas):
     self.numero = pconcurso
-    self.data = pdata  
+    self.data = pdata
     self.dezenas = pdezenas
-      
+
   def __str__(self):
     snumero  = str(self.numero)
     sdata    = str(self.data)
     sdezenas = str([str(x).zfill(2) for x in sorted(self.dezenas)])
-    return  snumero.rjust(4) +' '+ sdata +' '+ sdezenas  
-  @classmethod  
-  def get_online(self, nr_concurso=0):
-    def extrai_dados(texto):
-      if texto[:texto.find('|')] == '':
-        return Concurso('','','')
-      
-      vNumero = int(texto[:texto.find('|')])
-      
-      vdezenas = texto[texto.find('<ul>'):texto.find('</ul>')]
-      vdezenas = vdezenas.replace('<ul><li>','')
-      vdezenas = vdezenas.replace('</li><li>',';')
-      vdezenas = vdezenas.replace('</li>','')
-      vdezenas = vdezenas.split(';')  
-      vdezenas = [int(x) for x in vdezenas]
-      
-      texto = texto[texto.rfind('</a>'):]
-      data = texto.split('|')
-      if data[1] == '' :
-        vData = data[2]
-      else:
-        vData = data[1]
-        
-      return Concurso(vNumero, vData, vdezenas)
-    
-    #ultimo concurso:
-    url_uc="http://www1.caixa.gov.br/loterias/loterias/megasena/\
-    megasena_pesquisa_new.asp?f_megasena="
-    
-    #http://www1.caixa.gov.br/loterias/loterias/..megasena/...
-    #megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso=1270
-    url="http://www1.caixa.gov.br/loterias/loterias/megasena/\
-    megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso="
-    
-    if nr_concurso == 0:  
-      for line in urllib2.urlopen(url_uc):
-        ultimo_concurso = extrai_dados(line)
-        return ultimo_concurso
-    else:  
-      for line in urllib2.urlopen(url+str(nr_concurso)):
-        vConcurso = extrai_dados(line) 
-        return vConcurso  
+    return  snumero.rjust(4) +' '+ sdata +' '+ sdezenas
+
+
+def get_online(nr_concurso=0, sess=requests.session()):
+  def extrai_dados(texto):
+    if texto[:texto.find('|')] == '':
+      return Concurso('','','')
+
+    vNumero = int(texto[:texto.find('|')])
+
+    vdezenas = texto[texto.find('<ul>'):texto.find('</ul>')]
+    vdezenas = vdezenas.replace('<ul><li>','')
+    vdezenas = vdezenas.replace('</li><li>',';')
+    vdezenas = vdezenas.replace('</li>','')
+    vdezenas = vdezenas.split(';')
+    vdezenas = [int(x) for x in vdezenas]
+
+    texto = texto[texto.rfind('</a>'):]
+    data = texto.split('|')
+    if data[1] == '' :
+      vData = data[2]
+    else:
+      vData = data[1]
+
+    return Concurso(vNumero, vData, vdezenas)
+
+  if nr_concurso==0:
+    url="http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?f_megasena=/"
+  else:
+    url="http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso="+str(nr_concurso)
+  r = sess.get(url)
+  #print r.text
+  vConcurso = extrai_dados(r.text)
+  return vConcurso
 
 def carregar_concursos(arquivo=ARQUIVO):
   dic_concursos={}
@@ -64,12 +57,12 @@ def carregar_concursos(arquivo=ARQUIVO):
     with open(arquivo) as f:
       while True:
         try:
-          e = pickle.load(f)          
+          e = pickle.load(f)
           dic_concursos[e.numero] = e
         except EOFError:
           break
   except IOError:
-    print 'Nao existem dados a carregar.'    
+    print('Nao existem dados a carregar.')
   return dic_concursos
 
 def salvar_concursos(dic_concursos,arquivo=ARQUIVO):
@@ -78,13 +71,15 @@ def salvar_concursos(dic_concursos,arquivo=ARQUIVO):
       pickle.dump(dic_concursos[e],f)
 
 def atualizar_concursos(dic_concursos, arquivo=ARQUIVO):
-  ultimo = Concurso.get_online()
+  s=requests.session()
+  ultimo = get_online(0,s)
   if len(dic_concursos) == 0:
     primeiro = 1
   else:
     primeiro = len(dic_concursos)+1
   for nr in range(primeiro, ultimo.numero+1):
-    conc = Concurso.get_online(nr)
+    print('Recuperando sorteio nr '+str(nr))
+    conc = get_online(nr,s)
     dic_concursos[nr]=conc
   return dic_concursos
 
@@ -100,7 +95,12 @@ def gerar_jogo():
       jogo.clear()
   return sorted(list(jogo))
 
-e = Concurso.get_online()
-print e
-
-
+if __name__ == "__main__":
+  print("Carregando variavel concursos")
+  concursos = carregar_concursos()
+  print("Atualizando concursos")
+  concursos = atualizar_concursos(concursos)
+  print("Ultimo sorteio:", len(concursos))
+  print(concursos[len(concursos)])
+  salvar_concursos(concursos)
+  print("Para imprimir algum sorteio: print(concursos[sorteio])")
