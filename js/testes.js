a3bebeeea5e5ef47ec9abccaf58bca373faeabe1
clairr <clair.rios@gmail.com>
2016-12-15 01:00:00 -0200

    Tested module request

--- js/testes.js
+++ js/testes.js
@@ -1,2 +1,11 @@
-var foo = {bar: 'foobar'};
-console.log('Hello %s, this is my object: %j', 'World', foo);
+var ultimo="http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?f_megasena="
+var primeiro= "http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso=1"
+
+var request = require('request');
+request({uri: primeiro, jar:true, encoding: 'latin1'}, function(error, req, body){
+  if( !error ) {
+  	console.log(body)
+
+  };	
+});
+
