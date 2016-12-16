31bb6e73e5e03fdbfeef2c0756d511593f245c9c
clairr <clair.rios@gmail.com>
2016-12-16 16:11:30 -0200

    Tested file api

--- js/megasena.js
+++ js/megasena.js
@@ -1,49 +1,41 @@
-var ultimo = "http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?f_megasena="
-
-
-function getDezenas(texto) {
-	var textoDezenas=texto.substring(texto.indexOf('<li>'),texto.indexOf('</ul>'));
-	var re = /<li>(\d+)<\/li>/g
-	var dezenas = []
-
-	for(i=0; i<6; i++){
-	    strNumero=re.exec(textoDezenas)[1];
-	    dezenas.push(parseInt(strNumero));
-    }
-    return dezenas;
-};
-
-
-var concursos=[];
-var ultimoErro;
-var totalErros=0;
-
-var request = require('request');
 
 function getConcursosOnline(numeroConcurso, resultArray){
-	var primeiro = "http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso="
-	request({uri: primeiro+numeroConcurso, jar:true, encoding: 'latin1'}, function(error, response, data){
-		if( !error && response.statusCode==200) {
-			//var numeroConcurso = parseInt(data.substr(0, data.indexOf('|')));
-			var dataConcurso = data.match(/.*<\/a>\|(\d+\/\d+\/\d+)/)[1]
-			var dezenas=getDezenas(data);
-			//var concurso = {"numero": numeroConcurso, "data": dataConcurso, "dezenas": dezenas};
-			console.log("processando concurso "+numeroConcurso);
-			//console.log(concurso);
-			//concursos.push(concurso);
-			resultArray.push({"numero": numeroConcurso, "data": dataConcurso, "dezenas": dezenas});
-			//console.log(dezenas);
 
-		} else {
-			//totalErros+=1;
-			//ultimoErro=error;
-			resultArray.push(error);
-		};	
-	});
+	// extrai dezenas do texto html retornado
+	function getDezenas(texto) {
+		var textoDezenas=texto.substring(texto.indexOf('<li>'),texto.indexOf('</ul>'));
+		var re = /<li>(\d+)<\/li>/g
+		var dezenas = []
+
+		for(i=0; i<6; i++){
+			strNumero=re.exec(textoDezenas)[1];
+			dezenas.push(parseInt(strNumero));
+		}
+		return dezenas;
+	};
+
+  var request = require('request');
+  var primeiro = "http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso=";
+  var ultimo   = "http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?f_megasena=";
+  var target;
+
+  if(numeroConcurso!=0) 
+    {target = primeiro+numeroConcurso}
+  else
+    {target = ultimo};
+  console.log(target);
+
+  request({uri: target, jar:true, encoding: 'latin1'}, function(error, response, data){
+    if( !error && response.statusCode==200) {    	
+      var dataConcurso = data.match(/.*<\/a>\|+(\d+\/\d+\/\d+)/)[1]
+	    var dezenas=getDezenas(data);
+	    if(numeroConcurso==0) numeroConcurso=data.substring(0, data.indexOf('|'));
+	    console.log("processando concurso "+numeroConcurso);
+	    resultArray[numeroConcurso] = {"numero": numeroConcurso, "data": dataConcurso, "dezenas": dezenas};
+    } else {
+	  resultArray[0] = error;
+	};	
+  });
 };
 
 var texto='8|4.493.748,19|<span class="num_sorteio"><ul><li>53</li><li>17</li><li>38</li><li>04</li><li>47</li><li>37</li></ul></span>|0|0,00|60|16.084,11|5.262|183,40|<a class="btn_conc_ant_megasena" href="javascript:carrega_concurso(7);" tabindex="27" title="Ver concurso anterior">Ver concurso anterior</a>|<a class="btn_conc_prx_megasena" href="javascript:carrega_concurso(9);" tabindex="27" tilte="Ver próximo concurso">Ver próximo concurso</a>|29/04/1996|Brasília|DF|A||||0,00||<span class="num_sorteio"><ul><li>04</li><li>17</li><li>37</li><li>38</li><li>47</li><li>53</li></ul></span>||||';
-var texto='so</a>|29/04/1996|Brasília|DF|A||||0,00||<spli>47</li><li>53</li></ul></span>||||';
-
-
-
--- js/testes.js
+++ js/testes.js
@@ -1,40 +1,32 @@
-var ultimo="http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?f_megasena="
-var primeiro= "http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso="
+var fs=require('fs')
+function loadConcursos() {
+	var data,
+	concursos;
 
-function getDezenas(texto) {
-	var textoDezenas=texto.substring(texto.indexOf('<li>'),texto.indexOf('</ul>'));
-	var re = /<li>(\d+)<\/li>/g
-	var dezenas = []
+	try {
 
-	for(i=0; i<6; i++){
-	    //dezenas.push(re.exec(textoDezenas)[1]);
-	    strNumero=re.exec(textoDezenas)[1];
-	    dezenas.push(parseInt(strNumero));
+		data = fs.readFileSync('concursos.txt'); 	
+		try{
+			concursos = JSON.parse(data);
+		} catch (err) {
+			console.log(err);
+		}
+		return concursos;
+
+	} catch (err) {
+ 	  if (err.errno==-2) { //no such file or directory
+ 		return concursos =[];
+ 	  } else {
+ 		console.log(err);
+ 	  }
     }
-    return dezenas;
-}
-
-var concursos={lista:[]};
-var ultimoErro;
-var totalErros=0;
-
-var request = require('request');
-for (i=1; i<20; i++) {
-	request({uri: primeiro+i, jar:true, encoding: 'latin1'}, function(error, req, data){
-		if( !error ) {
-			console.log("processando concurso "+ data.substr(0, data.indexOf('|')));
-			var dezenas=getDezenas(data);
-			concursos.lista.push(dezenas);
-			console.log(dezenas);
-
-		} else {
-			totalErros+=1;
-			ultimoErro=error;
-		};	
-	});
 };
 
-var texto='8|4.493.748,19|<span class="num_sorteio"><ul><li>53</li><li>17</li><li>38</li><li>04</li><li>47</li><li>37</li></ul></span>|0|0,00|60|16.084,11|5.262|183,40|<a class="btn_conc_ant_megasena" href="javascript:carrega_concurso(7);" tabindex="27" title="Ver concurso anterior">Ver concurso anterior</a>|<a class="btn_conc_prx_megasena" href="javascript:carrega_concurso(9);" tabindex="27" tilte="Ver próximo concurso">Ver próximo concurso</a>|29/04/1996|Brasília|DF|A||||0,00||<span class="num_sorteio"><ul><li>04</li><li>17</li><li>37</li><li>38</li><li>47</li><li>53</li></ul></span>||||';
-
-
-
+function saveConcursos(concursos){
+	var data = JSON.stringify(concursos);
+	fs.writeFile('concursos.txt', data, function(err){
+		if (err) {
+			console.log('Error saving data.' + err.message)
+		}
+	});
+};
